af341eb33bcb3580f59532623b60b6658513e66d
glamourAni <glamourani@gmail.com>
2022-09-21 20:26:25 -0700

    Bug fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcat - appends the src string to the dest string, overwriting the
@@ -24,4 +25,23 @@ char *_strcat(char *dest, char *src)
 	dest[i + j] = '\0';
 
 	return (dest);
+}
+
+/**
+ * main - check the code
+ * Return: 0
+ */
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
+
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
+	return (0);
 }
