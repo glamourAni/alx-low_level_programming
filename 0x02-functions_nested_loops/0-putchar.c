a6b7c663010cff646fca5875ca144cc5883dde72
glamourAni <glamourani@gmail.com>
2022-09-22 21:31:37 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar, followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	char p;
+
+	p = '_putchar';
+	_putchar(p);
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_INCLUDED
+#define main_h_included
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+
+
+#endif
