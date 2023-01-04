af99191a47e460c25d18a73046aef3a9fe808037
glamourAni <glamourani@gmail.com>
2023-01-03 21:07:24 -0800

    First task and README

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - fills memory with constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: the first n bytes of @s
+ * Return: a pointer to @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
