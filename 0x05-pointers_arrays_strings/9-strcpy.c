c421990f8b8dc73283f9787268ec1dfe23feb166
glamourAni <glamourani@gmail.com>
2023-01-03 20:38:19 -0800

    updated two mandatory tasks`

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string, including the terminating null byte
+ * from one pointer to another
+ * @dest: the destination pointer
+ * @src: the sender pointer
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *p = dest;
+
+	while (*p++ = *src++)
+		return (dest);
+}
