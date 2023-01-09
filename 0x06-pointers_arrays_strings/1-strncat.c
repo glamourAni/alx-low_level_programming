835b57c20760a9071ca3b000b054fda0831326d0
glamourAni <glamourani@gmail.com>
2023-01-09 04:26:26 -0800

    fixed checker error  for task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,39 +12,20 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	/**
-	 * Find the destination string
-	 */
-	while (*dest != '\0')
-	{
-		dest++;
-	}
+	int dest_len = 0;
+	int i;
 
-	while (n--)
+	/* find the length of dest*/
+	while (dest[dest_len] != '\0')
 	{
-		if (!(*dest++ = *src++))
-			return (dest++);
+		dest_len = dest_len + 1;
 	}
-	dest = '\0';
+	/* get the n src bytes*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
 
 	return (dest);
-}
-
-int main(void)
-{
-	char s1[98] = "Hello ";
-	char s2[] = "World!\n";
-	char *ptr;
-
-	printf("%s\n", s1);
-	printf("%s", s2);
-	ptr = _strncat(s1, s2, 1);
-	printf("%s\n", s1);
-	printf("%s", s2);
-	printf("%s\n", ptr);
-	ptr = _strncat(s1, s2, 1024);
-	printf("%s", s1);
-	printf("%s", s2);
-	printf("%s", ptr);
-	return (0);
 }
