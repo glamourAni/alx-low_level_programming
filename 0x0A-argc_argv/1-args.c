ed60e27279f3c76785f268eacfec0e559185e404
glamourAni <glamourani@gmail.com>
2023-01-11 13:02:09 -0800

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+
+/**
+ * main - prints the number of arguments ppassed into it
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0(Success)
+ */
+int main(int argc, char __attribute__ ((unused)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
