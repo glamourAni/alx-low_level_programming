326b401efd227962bf803f67a927ba0a7b741ce3
glamourAni <glamourani@gmail.com>
2023-01-10 20:45:57 -0800

    fixed betty errors

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,18 +8,19 @@
  * @c: characters
  * Return: a pointer to the array or NULL if it fails
  */
-char *create_array(unsigned int size, char c) {
+char *create_array(unsigned int size, char c)
+{
 	unsigned int i;
-	char *array = (char*)malloc(sizeof(char) * size);
+	char *array = (char *)malloc(sizeof(char) * size);
 
 	if (size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	if (array == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
 	for (unsigned int i = 0; i < size; i++)
