6fcd72c27c4189f4d1536da8838eb598a34be4f2
glamourAni <glamourani@gmail.com>
2023-01-11 04:26:09 -0800

    recreated archive to fix bugs

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,23 +1,12 @@
 #include "main.h"
+#include "stdio.h"
 
 /**
-*main - Entry point
-*
-*Return: nothing, return void
-*/
-
-int main(void)
-
+ * _putchar - prints a character
+ * @c: character argument
+ * Return: int
+ */
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
 	return (0);
 }
