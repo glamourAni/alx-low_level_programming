028542f91cd0a36a2cfe8d9a57bcbefb5036f29a
glamourAni <glamourani@gmail.com>
2023-01-12 04:31:18 -0800

    fixed checker error for task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,46 +12,47 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
+	unsigned int len1 = 0, len2 = 0;
+	char *result = malloc(len1 + n + 1);
 
-    unsigned int len1 = 0, len2 = 0;
-    while (s1[len1])
-    {
-        ++len1;
-    }
-    while (s2[len2])
-    {
-        ++len2;
-    }
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
-    if (n >= len2)
-    {
-        n = len2;
-    }
+	while (s1[len1])
+	{
+		++len1;
+	}
+	while (s2[len2])
+	{
+		++len2;
+	}
 
-    char *result = malloc(len1 + n + 1);
-    if (result == NULL)
-    {
-        return NULL;
-    }
+	if (n >= len2)
+	{
+		n = len2;
+	}
 
-    for (unsigned int i = 0; i < len1; ++i)
-    {
-        result[i] = s1[i];
-    }
-    for (unsigned int i = 0; i < n; ++i)
-    {
-        result[i + len1] = s2[i];
-    }
-    result[len1 + n] = '\0';
+	if (result == NULL)
+	{
+		return NULL;
+	}
 
-    return result;
+	for (unsigned int i = 0; i < len1; ++i)
+	{
+		result[i] = s1[i];
+	}
+	for (unsigned int i = 0; i < n; ++i)
+	{
+		result[i + len1] = s2[i];
+	}
+	result[len1 + n] = '\0';
+
+	return result;
 }
 
